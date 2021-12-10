a6afb0be34c0d64f26dc6c94a090ca9d5303ba94
Villegas545 <villegas_545@hotmail.com>
2021-12-10 09:17:51 -0700

    Frontend test point 5 ready

--- js/clickFunctions.js
+++ js/clickFunctions.js
@@ -1,3 +1,7 @@
-$(document).on("click", "#btnWiki", function () {
-  alert($(this).attr("name"));
+$(document).on("click", ".trWikki", async function () {
+    $(".loader").show();
+    const nameCountry=$(this).attr("name");
+    const wiki = await $.get(`https://en.wikipedia.org/api/rest_v1/page/summary/${nameCountry}`);
+    $(".loader").hide();
+  bootbox.alert(wiki.extract_html)
 });
--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,15 @@
 //FRONTEND POINT 1
 const getCountries = async () => {
   const countries = await $.get("https://restcountries.com/v3.1/all");
-  // console.log(countries);
   countries.sort((a, b) => a.name.official.localeCompare(b.name.official));
   $(".loader").hide();
   return countries;
 };
 
 const writeTableCountries = (countries) => {
-  // $("#root").html(`
-  console.log(countries);
+ 
   let html = `
-            <table class="table table-striped">
+            <table class="table table-striped myTable" id="countryTable">
                 <thead>
                     <tr>
                       
@@ -28,7 +26,7 @@ const writeTableCountries = (countries) => {
 
   countries.map(
     (country) =>
-      (html += `
+      (html += `<tr class="trWikki" name=${country.name.common}>
                             <input type="hidden" value=${country.cca3} />
                             <td>${country.name.official}</td>
                             <td>${country.capital}</td>
@@ -39,12 +37,13 @@ const writeTableCountries = (countries) => {
                             </td>
                             <td>${country.population}</td>
                             <td><img src=${country.flags.png} alt="" width="70" height="35" /></td>
-                            <td><button class="btn btn-primary btn-sm" id="btnWiki" name=${country.name.common} >Wikki</button></td>
                         </tr>`)
   );
   html += `
                 </tbody>
-                </table>
+                   </table>
                 `;
-  $("#root").html(html);
+  $("#root").append(html);
+       paginate.init('.myTable');
 };
+
